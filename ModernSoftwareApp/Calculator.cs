1363a43e7817f7a49bd8b91cce03e46b3e318513
BEN-YOUNES-Ihab <ihab.ben.younes@hotmail.com>
2025-04-17 16:50:50 +0200

    format fix

--- ModernSoftwareApp/Calculator.cs
+++ ModernSoftwareApp/Calculator.cs
@@ -1,11 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿// <copyright file="Calculator.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
 
 namespace ModernSoftwareApp
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
     public class Calculator
     {
         public int Add(int a, int b) => a + b;
